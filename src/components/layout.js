69740f3e60e56ccd111dde13e3f23aa2087ebd29
Saravanakumar M <51469@hexaware.com>
2021-09-19 05:36:03 +0530

    Added StaticImage in index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from 'React'
 import { Link } from 'gatsby'
+
 import {
     container,
     heading,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
-import  { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I am using Gatsby tutorial to develop this page :)</p>
+      <StaticImage alt="hanging wood" src="../images/wooden-hanging.jpg"></StaticImage>
     </Layout>
+
   )
 }
 
